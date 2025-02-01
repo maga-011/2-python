ec700cf09c80188477ea6c42a008193bccbb7ed1
maga-011 <muhamedkarabagli08@gmail.com>
2025-02-01 19:51:03 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-import os
 from ext import create_app
-
+from routes import*
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Use Railway's assigned port
-    app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0",)
